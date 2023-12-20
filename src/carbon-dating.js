e89837304a16f6f3e852e9b787b6222bbd906ef8
Evgeniya Zhavoronkova <maongaku@gmail.com>
2023-12-20 12:21:53 +0300

    feat: add solutions for count-cats and carbon-dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  sampleActivity = Number(sampleActivity);
+  if (isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+  let age =
+    (Math.log(MODERN_ACTIVITY / sampleActivity) / Math.LN2) * HALF_LIFE_PERIOD;
+  return Math.ceil(age);
 }
 
 module.exports = {
